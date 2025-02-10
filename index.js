e19132dd2cd439821a9092263f5029de0ef95dfd
arelileonnunez <nem.arelil@coppel.com>
2025-02-10 10:44:46 -0600

    archivo nuevo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function funcion() {
   console.log("Hola (commit)")
 }
 const main = () => {
-  console.log('Inicio 21/01/2025')
+  console.log('Inicio 21/02/2025')
   log.warn("unreasonably simple");
   funcion()
   cursoGit()
